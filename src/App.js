a13cd9e0d7d3e6adbf886b5268af6fb9808327cb
mutebipaul <johnpaulmutebi@gmail.com>
2023-03-07 12:13:50 +0300

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCrad from "./ProfileCrad";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+function App(){
+    return(
+        <div>
+            <div>
+            <section className = "hero is-primary">
+                <div className = "hero-boby">
+                    <p classsName = "title">Personal Digital Assitant</p>
+                </div>
+            </section>
+            </div>
+            
+            <div className = "container">
+                <section classssName ="section" >
+                    <div className = "columns">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                        <div className = "column is-3">
+                            <ProfileCrad 
+                            title="Alexa"
+                             handle="@alexa99"
+                             image={AlexaImage}
+                             description ="Alexa was created by Amazon and helps you buy things"
+                             />
+                        </div>
+
+                        <div className = "column is-3">
+                            <ProfileCrad 
+                            title="Cortana"
+                             handle="@cortana32"
+                             image={CortanaImage}
+                             description = "Cortana was made by Microsoft.Who knows what it does?."
+                             />
+
+                        </div>
+
+                        <div className = "column is-3">
+                            <ProfileCrad
+                            title="Siri"
+                            handle="@siri01"
+                            image={SiriImage}
+                            description = "Siri was made by apple and is pharsed out."  
+                              />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            
+           
+            
+            
+            
+        </div>
+          
+
+
+            
+
+    );
 }
-
 export default App;
---
+++ src/ProfileCrad.js
@@ -0,0 +1,23 @@
+function ProfileCrad({title,handle,image,description}){
+    return(
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda_log"/>    
+                </figure>
+            
+             <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-3">{handle}</p>
+                    
+                </div>
+                <div className = "content"> {description}</div>
+            </div> 
+            </div>
+
+        </div>
+     );
+}
+ 
+export default ProfileCrad;
